81d5012362d8de2744f6aa340a8d2a3eab417a02
Antonio Melo Magalhães <64669309+anmagalhes@users.noreply.github.com>
2024-01-08 07:48:24 -0300

    atualizado

--- erp/app/dashboard/layout.jsx
+++ erp/app/dashboard/layout.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import Sidebar from '../ui/dashboard/Sidebar/sidebar.jsx';
 import Navbar from '../ui/dashboard/Navbar/navbar.jsx';
 
+import style from '../ui/dashboard/dashboard.module.css';
+
 const Layout = ({ children }) => {
   return (
-    <div>
-      <div>
+    <div className={style.container}>
+      <div className={style.menu}>
         <Sidebar />
       </div>
-      <div>
+      <div className={style.content}>
         <Navbar />
         {children}
       </div>
--- erp/app/ui/dashboard/Sidebar/sidebar.jsx
+++ erp/app/ui/dashboard/Sidebar/sidebar.jsx
@@ -1,7 +1,87 @@
-const Sidebar = () => {
-    return (
-        <div>Sidebar</div>
-    );
-  };
+import React from 'react';
+import style from './sidebar.module.css';
+import {
+    MdDashboard,
+    MdSupervisedUserCircle,
+    MdShoppingBag,
+    MdAttachMoney,
+    MdWork,
+    MdAnalytics,
+    MdPeople,
+    MdOutlineSettings,
+    MdHelpCenter,
+    MdLogout,
+  } from "react-icons/md";
   
-  export default Sidebar;
+  const menuItems = [
+    {
+      title: "Pages",
+      list: [
+        {
+          title: "Dashboard",
+          path: "/dashboard",
+          icon: <MdDashboard />,
+        },
+        {
+          title: "Users",
+          path: "/dashboard/users",
+          icon: <MdSupervisedUserCircle />,
+        },
+        {
+          title: "Products",
+          path: "/dashboard/products",
+          icon: <MdShoppingBag />,
+        },
+        {
+          title: "Transactions",
+          path: "/dashboard/transactions",
+          icon: <MdAttachMoney />,
+        },
+      ],
+    },
+    {
+      title: "Analytics",
+      list: [
+        {
+          title: "Revenue",
+          path: "/dashboard/revenue",
+          icon: <MdWork />,
+        },
+        {
+          title: "Reports",
+          path: "/dashboard/reports",
+          icon: <MdAnalytics />,
+        },
+        {
+          title: "Teams",
+          path: "/dashboard/teams",
+          icon: <MdPeople />,
+        },
+      ],
+    },
+    {
+      title: "User",
+      list: [
+        {
+          title: "Settings",
+          path: "/dashboard/settings",
+          icon: <MdOutlineSettings />,
+        },
+        {
+          title: "Help",
+          path: "/dashboard/help",
+          icon: <MdHelpCenter />,
+        },
+      ],
+    },
+  ];
+  
+const Sidebar = () => {
+  return (
+    <div className={style.container}>
+      Sidebar
+    </div>
+  );
+};
+
+export default Sidebar;
